95deef031e497a37306915258e2c38cb6ef037e2
Ray Kim <yomandawgshit@gmail.com>
2020-08-21 14:22:50 +0900

    Updates

---
+++ precache-manifest.83c6ce40e4988df9eacc839db7cb4016.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a89890e34eb9b4cddd035d1a1693792",
+    "url": "/react-pages/index.html"
+  },
+  {
+    "revision": "97e997880b28b9348241",
+    "url": "/react-pages/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6a10a78df6c76ca3a627",
+    "url": "/react-pages/static/js/2.b1b31c97.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-pages/static/js/2.b1b31c97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97e997880b28b9348241",
+    "url": "/react-pages/static/js/main.a1815801.chunk.js"
+  },
+  {
+    "revision": "acd97f9fe6a88d338c19",
+    "url": "/react-pages/static/js/runtime-main.096ef3ca.js"
+  }
+]);
